4a8e9eaaee5edf23a9283165e40e673ab28ed057
LEEKUMHO <30499772+LEEKUMHO@users.noreply.github.com>
2017-08-17 23:57:32 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,83 @@
+import java.security.MessageDigest;
+
+public class Main {
+    public static void main(String[] args) {
+        //     System.out.println("heello");
+
+        // operators
+        // Operators operators = new Operators();
+        // operators.ccalculate();
+       // PrimitiveDataTypes dataTypes = new PrimitiveDataTypes();
+        // dataTypes.explainInteger();
+        //dataTypes.explainDouble();
+        //dataTypes.castNumber();
+        //dataTypes.parseData();
+        //dataTypes.explainString();
+
+        //collection frameworks학습
+        //CollectionFrameworks collectionFrameworks = new CollectionFrameworks();
+        // collectionFrameworks.explainArray();
+        //collectionFrameworks.explainArrayList();
+        //collectionFrameworks.explainHashset();
+        //collectionFrameworks.explainHashMap();
+
+        //if-Statement
+        //IfStatement ifStatement = new IfStatement();
+        //ifStatement.explainBoolean();
+        //ifStatement.explainIfStatement();
+
+        //ForLoop
+        //ForLoop forLoop = new ForLoop();
+        //forLoop.explainForLoop();
+        //forLoop.explainForLoopwithArrayList();
+        //forLoop.explainwhile();
+
+        //Methods
+        //Methods methods = new Methods();
+        //methods.testMethodOne();
+        //methods.testMethodTwo("Two");
+        //methods.testMethodTwo("zero");
+        // methods.testMethodThree(); 반환은 헀지만 아무출력이 안됨.
+        //System.out.println(methods.testMethodThree());
+        //methods.testMethodFour(3,4);
+        //System.out.println(methods.testMethodFour(3,4));
+
+        //미니과제
+        //FavoriteFood favoriteFood = new FavoriteFood();
+        //favoriteFood.favoriteFood();
+        //favoriteFood.faveriteFood2("탕수육");
+        //System.out.println(favoriteFood.faveriteFood3());
+        //System.out.println(favoriteFood.faveriteFood4("짜장면 " ,"탕수육 "));
+
+        //Class
+        //Classes classes = new Classes(1,2);
+        //System.out.println(classes.sum());
+        //Classes classes2 = new Classes(2,3);
+        //System.out.println(classes2.sum());
+        //Classes classes3 = new Classes(2,3);
+        //System.out.println(classes3.multiply());
+
+        //SubClasses subClasses = new SubClasses(4,2);
+        //System.out.println(subClasses.sum());
+        //System.out.println(subClasses.multiply());
+        //System.out.println(subClasses.substract());
+        //System.out.println(subClasses.divide());
+
+        // access modifiers
+       // AccessModifiers accessModifiers = new AccessModifiers(5,7);
+        //accessModifiers.a = 5;
+        //accessModifiers.b = 7;
+        //System.out.println(accessModifiers.sum());
+        //System.out.println(accessModifiers.getA());
+        //accessModifiers.setA(10);
+        //System.out.println(accessModifiers);
+
+        //Test1(2017.08.17)
+        Test1 test1 = new Test1();
+        test1.Test1();
+        test1.Test2();
+        test1.Test3();
+        test1.Test4();
+
+    }
+}
---
+++ Test1.java
@@ -0,0 +1,26 @@
+public class Test1 {
+    public void Test1() {
+        int score = 95;
+        if (score >90){
+            System.out.println("A등급");
+        }
+    }
+   public void Test2() {
+        int score = 85;
+        if (score > 80){
+            System.out.println("B등급");
+        }
+   }
+   public void Test3(){
+        int score = 75;
+        if(score > 70){
+            System.out.println("C등급");
+        }
+   }
+   public void Test4(){
+       int score = 65;
+       if(score > 60){
+           System.out.println("F등급");
+       }
+   }
+}
