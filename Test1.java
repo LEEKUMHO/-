c04a9111f1a6e676716a203b86320abb6818648b
LEEKUMHO <30499772+LEEKUMHO@users.noreply.github.com>
2017-08-18 22:25:42 +0900

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,26 +1,21 @@
 public class Test1 {
     public void Test1() {
-        int score = 95;
-        if (score >90){
+        int score = 00;  //점수입력
+
+        if (score >= 90) {
             System.out.println("A등급");
+        } else if (score >= 80)
+            System.out.println("B등급");
+          else if (score >= 70)
+            System.out.println("C등급");
+          else if (score <= 69) {
+                System.out.println("F등급");
+            }
         }
     }
-   public void Test2() {
-        int score = 85;
-        if (score > 80){
-            System.out.println("B등급");
-        }
-   }
-   public void Test3(){
-        int score = 75;
-        if(score > 70){
-            System.out.println("C등급");
-        }
-   }
-   public void Test4(){
-       int score = 65;
-       if(score > 60){
-           System.out.println("F등급");
-       }
-   }
-}
+
+
+
+
+
+
