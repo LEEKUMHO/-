c4b5eba44370b9ebaabe5ba65f2d1f62196973e4
LEEKUMHO <30499772+LEEKUMHO@users.noreply.github.com>
2017-08-18 23:13:27 +0900

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,32 @@
+public class Test2 {
+    double number1 = 8;
+    double number2 = 18;
+
+    public void Test2() {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+    public double test1() {
+        return number1 + number2; }
+    { System.out.println(test1()); }
+
+    public double test2() {
+        return number1 - number2; }
+    { System.out.println(test2()); }
+
+    public double test3() {
+        return number1 / number2; }
+    { System.out.println(test3()); }
+
+    public double test4() {
+        return number1 * number2; }
+    { System.out.println(test4()); }
+}
+
+    
+
+
+
+
+
+
