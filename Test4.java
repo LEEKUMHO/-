2c75e7569169c02a619ea29401198b367ae25cf4
LEEKUMHO <30499772+LEEKUMHO@users.noreply.github.com>
2017-08-20 13:15:08 +0900

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Test4 {
+    public void Lotto(){
+        int[] Lotto = new int[25];
+        Lotto[0] = 1;
+        Lotto[1] = 2;
+        Lotto[2] = 3;
+        Lotto[3] = 4;
+        Lotto[4] = 5;
+        Lotto[5] = 6;
+        Lotto[6] = 7;
+        Lotto[7] = 8;
+        Lotto[8] = 9;
+        Lotto[9] = 10;
+        Lotto[10] = 11;
+        Lotto[11] = 12;
+        Lotto[12] = 13;
+        Lotto[13] = 14;
+        Lotto[14] = 15;
+        Lotto[15] = 16;
+        Lotto[16] = 17;
+        Lotto[17] = 18;
+        Lotto[18] = 19;
+        Lotto[19] = 20;
+        Lotto[20] = 21;
+        Lotto[21] = 22;
+        Lotto[22] = 23;
+        Lotto[23] = 24;
+        Lotto[24] = 25;
+
+        System.out.println((int)(Math.random()*((24)+1)));
+        System.out.println((int)(Math.random()*((24)+1)));
+        System.out.println((int)(Math.random()*((24)+1)));
+        System.out.println((int)(Math.random()*((24)+1)));
+        System.out.println((int)(Math.random()*((24)+1)));
+        System.out.println((int)(Math.random()*((24)+1)));
+
+
+
+    }
+
+
+
+    }
+
+
+
+
+
